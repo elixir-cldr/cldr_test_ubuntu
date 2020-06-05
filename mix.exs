1e64eef093bf6fd87d1a78a8ba5ec29ebb042e55
Kip Cole <kipcole9@gmail.com>
2020-06-06 04:44:26 +0800

    Elixir 1.8 is allowed

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TestCldr.MixProject do
     [
       app: :test_cldr,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
