6f6e97489c672e243605e46a68ad6b39a25f0d6b
Kip Cole <kipcole9@gmail.com>
2020-06-06 04:41:46 +0800

    Remove doc test

--- lib/test_cldr.ex
+++ lib/test_cldr.ex
@@ -3,6 +3,7 @@ defmodule TestCldr do
     default_locale: "en-CA",
     locales: ["en-CA", "fr-CA", "en", "fr"],
     precompile_number_formats: ["¤¤#,##0.##"],
-    providers: [Cldr.Calendar, Cldr.DateTime, Cldr.List, Cldr.Number]
+    providers: [Cldr.Calendar, Cldr.DateTime, Cldr.List, Cldr.Number],
+    generate_docs: false
 
 end
--- test/test_cldr_test.exs
+++ test/test_cldr_test.exs
@@ -1,8 +1,5 @@
 defmodule TestCldrTest do
   use ExUnit.Case
-  doctest TestCldr
 
-  test "greets the world" do
-    assert TestCldr.hello() == :world
-  end
+
 end
